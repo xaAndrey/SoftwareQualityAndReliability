d8bd5ae1266d2971d0df54c5a724d7575a279059
Andrey Pisarev <andrey-pisarev-0@mail.ru>
2022-07-19 15:27:28 +0300

    Implemented a method for finding a node and del it from the program code
    ImportNode class

--- SoftwareQualityAndReliability/ImportNode.cpp
+++ SoftwareQualityAndReliability/ImportNode.cpp
@@ -18,8 +18,25 @@ string& ImportNode::getName() {
 	return this->name;
 }
 
-list<string>& ImportNode::findAndDeleteFromStackNode(list<string>& textCode) {
-	// TODO - добавить реализацию
+list<string>& ImportNode::findAndDeleteFromStackNode(list<string>& textCode) {	
+	auto iterTextCode = textCode.begin(); // Создаем итератор, на начало списка
+
+	// Для всех элементов списка
+	for (; iterTextCode != textCode.end(); iterTextCode++) {
+		string sourceStr = *iterTextCode; // Создаем строку, в которую помещаем содержимое проекта
+
+		// Если в строке есть импорт
+		if (sourceStr.find("import") != string::npos) {
+			int indexStartNameImport = sourceStr.find(' '); // Находим начало имени импорта
+			int indexEndNameImport = sourceStr.find(';'); // Находим конец имени импорта
+			int lengthImport = indexEndNameImport - indexStartNameImport + 1; // Находим длину имени
+
+			string tmpStr = sourceStr.substr(indexStartNameImport, lengthImport); // Создаем временную строку и помещаем в неё имя импорта
+			this->setName(tmpStr); // Устанавливаем имя импорта
+
+			textCode.erase(iterTextCode); // Удаляем строку из текста программы
+		}
+	}
 	return textCode; // Возвращаем список строк
 }
 
