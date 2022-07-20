7c354bfb8eaff7a5ec490a6afb4584b2f32f8a06
Andrey Pisarev <andrey-pisarev-0@mail.ru>
2022-07-20 10:40:22 +0300

    Implemented a method for finding a node and del it from the program code
    PackageNote class

--- SoftwareQualityAndReliability/PackageNode.cpp
+++ SoftwareQualityAndReliability/PackageNode.cpp
@@ -18,7 +18,25 @@ string& PackageNode::getName() {
 
 list<string>& PackageNode::findAndDeleteFromStackNode(list<string>& textCode) {
 	// TODO - Добавить реализацию
-	return textCode; // Вернуть список строк
+	auto iterTextCode = textCode.begin(); // Создаем итератор, на начало списка
+
+	// Для всех элементов списка
+	for (; iterTextCode != textCode.end(); iterTextCode++) {
+		string sourceStr = *iterTextCode; // Создаем строку, в которую помещаем содержимое проекта
+
+		// Если в строке есть импорт
+		if (sourceStr.find("package") != string::npos) {
+			int indexStartNameImport = sourceStr.find(' '); // Находим начало имени импорта
+			int indexEndNameImport = sourceStr.find(';'); // Находим конец имени импорта
+			int lengthImport = indexEndNameImport - indexStartNameImport + 1; // Находим длину имени
+
+			string tmpStr = sourceStr.substr(indexStartNameImport, lengthImport); // Создаем временную строку и помещаем в неё имя импорта
+			this->setName(tmpStr); // Устанавливаем имя импорта
+
+			textCode.erase(iterTextCode); // Удаляем строку из текста программы
+		}
+	}
+	return textCode; // Возвращаем список строк
 }
 
 list<string>& PackageNode::getXMLView() {
